94951f01cbbc597a93ffffaf29b99adae0314af2
Wei Tang <tangwei@smail.nju.edu.cn>
2020-04-28 07:36:27 +0800

    fixes in postprocess

--- postprocess.py
+++ postprocess.py
@@ -12,7 +12,7 @@ torch.manual_seed(42)
 from cmpo import *
 import model
 
-import power_projection as power
+from power_projection import *
 
 if __name__=='__main__':
     import argparse
@@ -26,7 +26,7 @@ if __name__=='__main__':
     device = torch.device("cpu" if args.cuda<0 else "cuda:"+str(args.cuda))
     dtype = torch.float32 if args.float32 else torch.float64
 
-    chi = int(args.data.split('_chi')[1].split('_beta')[0])
+    bondD = int(args.data.split('bondD')[1].split('_beta')[0])
     beta = float(args.data.split('_beta')[1].split('_Gamma')[0])
     Gamma = float(args.data.split('_Gamma')[1].split('_J')[0])
     J = float(args.data.split('_J')[1].split('-meas')[0])
@@ -45,33 +45,19 @@ if __name__=='__main__':
     print('read data from step %g'%optim_step)
 
     cmpsdata_name = args.data[:-9]+'/psi_{:03d}.pt'.format(optim_step)
-    Q = torch.rand(chi, dtype=dtype, device=device)
-    R = torch.rand(d,chi,chi, dtype=dtype, device=device)
+    Q = torch.rand(bondD, dtype=dtype, device=device)
+    R = torch.rand(d,bondD,bondD, dtype=dtype, device=device)
     Q = torch.nn.Parameter(Q)
     R = torch.nn.Parameter(R)
     cmpsdata = data_cmps(Q, R)
     dataload(cmpsdata, cmpsdata_name)
     psi = cmps(torch.diag(Q), R).detach()
 
-    #chi_loc = power.chi(psi, W, T, s.Z, s.Z, beta) / beta
-    wrange = np.linspace(-0.1, 0.1, 21) + 1e-6
-    S0 = 0.01*np.sum([power.spectral(psi, W, T, s.Z, s.Z, beta, omega, eta=0.05) for omega in wrange])
+    Lpsi = multiply(W, psi)
+    Z_value = AL_entropy(psi, Lpsi, T, s.Z, beta)
 
     out = args.out
     f_out = io.open(out, 'a')
-    f_out.write('{:.4f}  {:.12f} \n'.format(1/beta, S0))
+    f_out.write('{:.4f}  {:.12f} \n'.format(1/beta, Z_value))
     f_out.close()
-
-#    import matplotlib 
-#    import matplotlib.pyplot as plt
-#    plt.yscale('log')
-#    plt.xlabel(r'$\omega$')
-#    plt.ylabel(r'$S_0$')
-
-#    omega_list = np.linspace(-0.1,0.1,101)+1e-6 
-#    for eta in [0.1, 0.05, 0.01, 0.001]:
-#        S_list = [power.spectral(psi, W, T, s.Z, s.Z, beta, omega, eta) for omega in omega_list]
-#        plt.plot(omega_list, np.abs(S_list), '-', markerfacecolor='none', label=r'$\eta={:.3f}$'.format(eta))
-#    plt.legend()
-#    plt.savefig('hello.pdf')
 
